ba912028fe92ac8e5370b34da003693227ec4c63
bk1000 <63239330+bk1000@users.noreply.github.com>
2020-10-28 15:26:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const youtube = new YouTube("AIzaSyB0HKdYA7DEAd_1Y8zW9qt7yviRgXDWLCc");
 const queue = new Map();
  
  
